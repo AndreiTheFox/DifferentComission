80517fc574284ba2a05ab7747a6511dd70905a11
Andrei Prokudin <andrewicehab@gmail.com>
2023-03-20 20:48:12 +0700

    Added Monthly checks Added monthly defaults

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,50 @@
 fun main() {
-    val payment: Int = 10_000
-    val totalMonthPayments = 1000
-    val dailyLimits: Int = 150_000
-    val monthlySend: Int = 600_000
-    val monthlyReceive: Int = 600_000
-    val paymentMethod: String = "VK Pay"
-    println(comissionCalculate(paymentAmount = payment, paymentMethod = paymentMethod))
-
+    val payment: Int = 10000
+    val paymentMethod: String = "Мир"
+    val totalMonthPayments: Float = 0F
+    println(comissionCalculate(paymentAmount = payment, paymentMethod = paymentMethod, totalMonthPayments = totalMonthPayments))
 }
 fun comissionCalculate (
     paymentMethod: String = "VK Pay",
-    totalMonthPayments: Int = 0,
+    totalMonthPayments: Float = 0F,
     paymentAmount: Int
 ): Float {
 val comission: Float = when (paymentMethod) {
     "VK Pay" -> vkPay(paymentAmount, totalMonthPayments)
-    "Visa", "Мир"-> visaMir(paymentAmount)
-    "Maestro",  "Mastercard" -> masterkardMaestro(paymentAmount)
-    else -> 0.00F
+    "Visa", "Мир"-> visaMir(paymentAmount, totalMonthPayments)
+    "Maestro",  "Mastercard" -> masterkardMaestro(paymentAmount, totalMonthPayments)
+    else -> -10.00F
     }
     return comission
 }
-fun visaMir(amount :Int): Float {
-    val minComission: Float = 35F //Минимальная комиссия
-    val comission: Float = 0.75F //Процент комиссии
-    val fee: Float = minComission + (amount/100*comission) //Комиссия 35 руб+0.75%
-    return fee
+fun visaMir(amount :Int, totalMonthMoney: Float = 0F): Float {
+    val maxSend: Float = 150_000F //Максимальный перевод в сутки
+    val maxSendMonth: Float = 600_000F //Максимум отправленых и полученных средств за месяц
+    val minComission: Float = 35F //Минимальная комиссия, оплачиваемая в любой транзакции
+    val comission: Float = 0.75F //Процент комиссии для расчёта при сделках
+    val countedComission = (amount / 100 * comission).toFloat()     //Рассчитываемая комиссия от сделки
+    val fee: Float = when {
+        countedComission<=minComission -> minComission
+        else -> countedComission
+    }
+    return if (totalMonthMoney<=maxSendMonth)  {
+        fee
+    }
+    else -1.00F
 }
-fun masterkardMaestro (amount: Int): Float {
+fun masterkardMaestro (amount: Int, totalMonthMoney: Float = 0F): Float {
     val promotion: Boolean = true
+    val maxSend: Float = 150_000F //Максимальный перевод в сутки
+    val maxSendMonth: Float = 600_000F //Максимум отправленых и полученных средств за месяц
     val fee: Float = if((amount in 300..75000)&&promotion) 0.00F else ((amount/1000)*6+20).toFloat()
-    return fee
+    return if (totalMonthMoney<=maxSendMonth)  {
+        fee
+    }
+    else -1.00F
 }
-fun vkPay(amount: Int, totalMonthPayments: Int): Float {           //Доступен ли платеж через VKpay
-    val maxSend: Int = 15_000
-    val maxSendMonth: Int = 40_000
-    return if ((amount<=maxSend) && (totalMonthPayments<=maxSendMonth)) 0.00F
-    else -10_000.00F
+fun vkPay(amount: Int, totalMonthPayments: Float = 0F): Float {           //Доступен ли платеж через VKpay
+    val maxSend: Int = 15_000      //Максимальная величина для отправки средств за 1 раз
+    val maxSendMonth: Int = 40_000 //Максимум отправленых средств за месяц
+    return if ((amount<=maxSend) && (totalMonthPayments<=maxSendMonth)) 0.00F //Если соблюдаются условия перевода
+    else 0.00F //Возможно увеличить комиссию в будущем, при выходе за условия
 }
